c9273035cf5cd11ce343fcec6d325efd23823079
jasonbringle <jasonbringle@gmail.com>
2020-08-11 19:18:31 -0700

    final commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,8 @@
 const bcryptjs = require("bcryptjs");
 
 const router = require("express").Router();
+const jwt = require("jsonwebtoken")
+const secrets = require("../config/secrets")
 
 const Users = require("../users/users-model.js");
 const { isValid } = require("../users/users-service.js");
@@ -39,7 +41,9 @@ router.post("/login", (req, res) => {
       .then(([user]) => {
         // compare the password the hash stored in the database
         if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
+          // Generate a token and include in response
+          const token = generateToken(user)
+          res.status(200).json({ message: "Welcome to our API", token});
         } else {
           res.status(401).json({ message: "Invalid credentials" });
         }
@@ -53,5 +57,22 @@ router.post("/login", (req, res) => {
     });
   }
 });
+
+function generateToken(user){
+
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role
+  }
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  const secret = secrets.jwtSecret;
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,4 +1,19 @@
+const jwt = require("jsonwebtoken")
+const secrets = require("../config/secrets.js")
+
 module.exports = (req, res, next) => {
-  // add code here to verify users are logged in
-  next();
+  const  [authType,token] = req.headers.authorization.split(" ");
+
+  if (token){
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err){
+          res.status(401).json({you: "cant touch this (invalid token)"})
+      } else {
+          req.decodedJwt = decodedToken;
+          next()
+      }
+    })
+  } else {
+    res.status(401).json({you: "have no power! (no teken indcluded)"})
+  }
 };
